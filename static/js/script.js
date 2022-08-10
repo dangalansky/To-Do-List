11bb1675935303a783e8e9f364ce91a61142201a
dan_hates_the_future <97214702+dangalansky@users.noreply.github.com>
2022-08-09 19:44:50 -0600

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 const tasks = document.getElementsByTagName("li")
 const removeBtn = document.querySelector('.remove-complete-btn');
+var array = [];
 
 function line(){
    for(let i = 0;i < tasks.length; i++)
@@ -7,16 +8,26 @@ function line(){
     tasks[i].addEventListener('click', function handleClick() {
         tasks[i].style.textDecoration = "line-through";
         tasks[i].style.color='red';
+        array.push(tasks[i]);
+        console.log(tasks[i]);
+
     });
 }
 }
 
-removeBtn.addEventListener("click", () => {
-    tasks.forEach(task => {
-        const checked = task.querySelector('input').checked;
-        if( checked ) {
-            task.remove();
-        }
-    });
-    //console.log(tasks);
-});
+//function removeComplete() {
+////    if array.includes(tasks[i]) {
+//        console.log('HEY!');
+//    }
+//}
+
+//removeBtn.addEventListener("click", () => {
+//    tasks.forEach(tasks => {
+//        const checked = tasks.querySelector('input').checked;
+//        if( checked ) {
+////            tasks.remove();
+//            console.log('checked!')
+//        }
+//    });
+//    //console.log(tasks);
+//});
