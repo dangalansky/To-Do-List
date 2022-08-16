1acc4f32cc5d3e722c467bbede6ac6d075ddc302
dan_hates_the_future <97214702+dangalansky@users.noreply.github.com>
2022-08-16 09:26:54 -0600

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,11 @@
 const tasks = document.getElementsByTagName("li")
 const removeBtn = document.querySelector('.remove-complete-btn');
-var array = [];
 
 function line(){
-   for(let i = 0;i < tasks.length; i++)
-{
-    tasks[i].addEventListener('click', function handleClick() {
-        tasks[i].style.textDecoration = "line-through";
-        tasks[i].style.color='red';
-        array.push(tasks[i]);
-        console.log(tasks[i]);
+   for(let i = 0;i < tasks.length; i++){
+    tasks[i].addEventListener('click', function() {
+    tasks[i].classList.toggle("linethrough");
 
     });
 }
 }
-
-//function removeComplete() {
-////    if array.includes(tasks[i]) {
-//        console.log('HEY!');
-//    }
-//}
-
-//removeBtn.addEventListener("click", () => {
-//    tasks.forEach(tasks => {
-//        const checked = tasks.querySelector('input').checked;
-//        if( checked ) {
-////            tasks.remove();
-//            console.log('checked!')
-//        }
-//    });
-//    //console.log(tasks);
-//});
